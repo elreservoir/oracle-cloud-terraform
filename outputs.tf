2fc0ff1cdb1e3e31312d8d7b4faf21c7558c63d0
Administrator <elreservoir+root@gmail.com>
2025-05-02 18:37:43 +0000

    save output to vault

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,20 @@
-/*
- * Export ssh private (tls_private_key.ssh.private_key_pem),
- * public key (tls_private_key.ssh.public_key_openssh)
- * and public ip (i.public_ip) to hashicorp vault
- */
+resource "vault_kv_secret_v2" "add_secrets" {
+  mount = "kv"
+  name  = "oracle-cloud/generated"
 
- /*
-  * Does this have to work for n instances
-  */
+  delete_all_versions = true
+
+  data_json = jsonencode(
+    merge(
+        {
+            SSH_PRIVATE_KEY = tls_private_key.ssh.private_key_pem
+            SSH_PUBLIC_KEY = tls_private_key.ssh.public_key_openssh
+            IP_ADDRESS_i = i.public_ip
+        },
+        {
+            for idx, inst in sort(oci_core_instance._) :
+            "IP_ADDRESS_${idx + 1}" => inst.public_ip
+        }
+    )
+  )
+}
