b6e004a343c07341bb02231f2baef9835149a8d9
Administrator <elreservoir+root@gmail.com>
2025-05-02 18:40:10 +0000

    fix issue

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,6 @@ resource "vault_kv_secret_v2" "add_secrets" {
         {
             SSH_PRIVATE_KEY = tls_private_key.ssh.private_key_pem
             SSH_PUBLIC_KEY = tls_private_key.ssh.public_key_openssh
-            IP_ADDRESS_i = i.public_ip
         },
         {
             for idx, inst in sort(oci_core_instance._) :
